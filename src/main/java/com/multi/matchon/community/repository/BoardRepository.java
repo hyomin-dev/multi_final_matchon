ed663434026a125c1ebc7ed430a475166a189306
wnsgur2358 <wnsgur6010@naver.com>
2025-05-21 02:21:22 +0900

    게시판 기본틀

--- src/main/java/com/multi/matchon/community/repository/BoardRepository.java
+++ src/main/java/com/multi/matchon/community/repository/BoardRepository.java
@@ -1,13 +1,10 @@
 package com.multi.matchon.community.repository;
 
-
 import com.multi.matchon.community.domain.Board;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
-    Page<Board> findByCategory(String category, Pageable pageable);
 }
+
 
 
